b2300e9dccaaa96e72f449a62b07ca291bddd0d7
Justyna Setlak (PGS Software) <jsetlak@pgs-soft.com>
2021-02-02 18:30:47 +0100

    thread safe sample

---
+++ ThreadSafeSample/ThreadSafeSample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSafeSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var minimumValue = 0;
+            var maximumValue = 30;
+            var maximumDegreeOfParallelism = 3;
+
+            var data = new ThreadSafeQueue<int>();
+
+            var integerList = Enumerable.Range(minimumValue, maximumValue).ToList();
+
+            Parallel.ForEach(integerList, new ParallelOptions { MaxDegreeOfParallelism = maximumDegreeOfParallelism }, specificValue =>
+            {
+                data.Enqueue(specificValue);
+                Console.Write($"\n Enque {specificValue}, threadId: {Thread.CurrentThread.ManagedThreadId}"); 
+                
+                if(specificValue%2 == 0)
+                {
+                    var value = data.Dequeue();
+                    Console.Write($"\n Deque {value}, threadId: {Thread.CurrentThread.ManagedThreadId}");
+                }
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ThreadSafeSample/ThreadSafeSample/ThreadSafeQueue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThreadSafeSample
+{
+    public class ThreadSafeQueue<T>
+    {
+        private List<T> data = new List<T>();
+        private object actionLock = new Object();
+
+        public T Dequeue()
+        {
+            T item = default;
+
+            lock (actionLock)
+            {
+                if (data.Any())
+                {
+                    item = data.First();
+                    data.Remove(item);
+                }
+            }
+
+            return item;
+        }
+
+        public void Enqueue(T entry)
+        {
+            lock (actionLock)
+            {
+                data.Add(entry);
+            }
+        }
+    }
+}
