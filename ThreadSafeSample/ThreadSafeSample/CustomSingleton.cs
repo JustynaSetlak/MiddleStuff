58a3791c02f4298d683d7e85fd5747ca25b1c740
Justyna Setlak <jsetlak@pgs-soft.com>
2021-03-09 20:38:23 +0100

    double check pattern

---
+++ ThreadSafeSample/ThreadSafeSample/CustomSingleton.cs
@@ -0,0 +1,23 @@
+﻿namespace ThreadSafeSample
+{
+    public class CustomSingleton
+    {
+        private static CustomSingleton _instance;
+        private static readonly object ActionLock = new object();
+
+        private CustomSingleton() { }
+
+        public static CustomSingleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (ActionLock)
+                {
+                    return _instance ??= new CustomSingleton();
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
